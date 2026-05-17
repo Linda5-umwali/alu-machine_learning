061128a16f61328f8e96d6c42b88edf66ea0676b
Linda5-umwali <l.umwali@alustudent.com>
2026-05-17 23:00:45 +0200

    Linear Algebra

---
+++ 2-size_me_please.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Module that defines matrix_shape"""
+
+
+def matrix_shape(matrix):
+    """Returns the shape of a matrix"""
+    shape = []
+    while type(matrix) is list:
+        shape.append(len(matrix))
+        matrix = matrix[0]
+    return shape
