e330c963fd81fa73e9d946c1b2972d01a2de67f9
Linda5-umwali <l.umwali@alustudent.com>
2026-07-21 19:41:58 +0200

    redirection

--- math/linear_algebra/0-slice_me_up.py
+++ math/linear_algebra/0-slice_me_up.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-arr = [1, 2, 3, 4, 5, 6, 7, 8]
+arr = [9, 8, 2, 3, 9, 4, 1, 0, 3]
 arr1 = arr[:2]
 arr2 = arr[-5:]
 arr3 = arr[1:6]
-print(arr1)
-print(arr2)
-print(arr3)
+print("The first two numbers of the array are: {}".format(arr1))
+print("The last five numbers of the array are: {}".format(arr2))
+print("The 2nd through 6th numbers of the array are: {}".format(arr3))
