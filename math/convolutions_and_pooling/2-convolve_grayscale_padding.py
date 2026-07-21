d85d1e7d4df650b45ae9969ed21576861a8436b2
Linda5-umwali <l.umwali@alustudent.com>
2026-07-21 20:14:20 +0200

    Convolution with Padding

--- math/convolutions_and_pooling/2-convolve_grayscale_padding.py
+++ math/convolutions_and_pooling/2-convolve_grayscale_padding.py
@@ -9,6 +9,26 @@ import numpy as np
 
 
 def convolve_grayscale_padding(images, kernel, padding):
+    '''
+        A function def convolve_grayscale_padding(images, kernel, padding):
+
+        Args:
+            images is a numpy.ndarray with shape (m, h, w)
+            containing multiple grayscale images
+            m is the number of images
+            h is the height in pixels of the images
+            w is the width in pixels of the images
+            kernel is a numpy.ndarray with shape (kh, kw)
+            containing the kernel for the convolution
+            kh is the height of the kernel
+            kw is the width of the kernel
+            padding is a tuple of (ph, pw)
+            ph is the padding for the height of the image
+            pw is the padding for the width of the image
+        Returns:
+            a numpy.ndarray containing
+            the convolved images
+    '''
     m = images.shape[0]
     height = images.shape[1]
     width = images.shape[2]
